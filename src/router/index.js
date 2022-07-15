5b7860092de678754ee6945ef6d388eb9f1e6016
fan379660107 <379660107@qq.com>
2022-07-15 17:53:07 +0800

    完成布局页面路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,29 @@ const routes = [
   {
     path: '/login',
     component: () => import('@/views/Login') // 懒加载写法直接在规则中导入
+  },
+  {
+    path: '/',
+    component: () => import('@/views/layout'),
+    redirect: '/ ',
+    children: [
+      {
+        path: '/ ',
+        component: () => import('@/views/Home')
+      },
+      {
+        path: '/video',
+        component: () => import('@/views/Video')
+      },
+      {
+        path: '/qa',
+        component: () => import('@/views/QA')
+      },
+      {
+        path: '/profile',
+        component: () => import('@/views/My')
+      }
+    ]
   }
 ]
 
---
+++ src/views/layout/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item to="/ ">
+        <template #icon>
+          <i class="iconfont icon-shouye"></i>
+          <span class="text">首页</span>
+        </template>
+      </van-tabbar-item>
+
+      <van-tabbar-item to="/video">
+        <template #icon>
+          <i class="iconfont icon-shipin"></i>
+          <span class="text">视频</span>
+        </template>
+      </van-tabbar-item>
+
+      <van-tabbar-item to="/qa">
+        <template #icon>
+          <i class="iconfont icon-wenda"></i>
+          <span class="text">问答</span>
+        </template>
+      </van-tabbar-item>
+
+      <van-tabbar-item to="/profile">
+        <template #icon>
+          <i class="iconfont icon-wode"></i>
+          <span class="text">我的</span>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+:deep(.van-tabbar-item__icon) {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 100%;
+  .iconfont {
+    font-size: 0.53333rem;
+  }
+  .text {
+    font-size: 0.32rem;
+  }
+}
+</style>
